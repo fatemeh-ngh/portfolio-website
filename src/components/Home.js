fa47916c1ce9196855554fb39c4d84df971d25a5
Fatemeh Negahdari <ftm.ngh.2001@gmail.com>
2022-07-19 21:41:18 +0430

    style navbar and add fonts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components'
+
 //Images
 import background from "../assets/images/background.jpg";
 
@@ -8,6 +8,7 @@ const Home = () => {
 
     return (
         <div>
+            <img src={background} alt="laptop" style={{width: "100%", height: "100vh"}}/>
             <img src={background} alt="laptop" style={{width: "100%", height: "100vh"}}/>
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+//Styles 
+import styles from'../assets/styles/Navbar.module.css';
+
 const Navbar = () => {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/portfolio">Portfolio</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/shop">Shop</Link>
-            <Link to="/contact">Contact</Link>
+        <div className={styles.container}>
+            <div className={styles.socilaContainer}>
+                <p>FN<span>.</span></p>
+            </div>
+            <div className={styles.linkContainer}>
+                <Link to="/">Home</Link>
+                <Link to="/portfolio">Portfolio</Link>
+                <Link to="/blog">Blog</Link>
+                <Link to="/shop">Shop</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+
         </div>
     );
 };
