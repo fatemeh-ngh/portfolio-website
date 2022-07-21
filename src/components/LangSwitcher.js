6c4cd123504f6894f3c7f096b368ac5eccaed80f
Fatemeh Negahdari <ftm.ngh.2001@gmail.com>
2022-07-21 17:06:49 +0430

    add the i18next package to make language switcher

--- src/components/LangSwitcher.js
+++ src/components/LangSwitcher.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-const LangSwitcher = () => {
-    return (
-        <div>
-            <select placeholder='choose your language'>
-                <option value="eng">English</option>
-                <option value="fa">فارسی</option>
-            </select>
-        </div>
-    );
-};
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    detection: {
+        order: [ 'htmlTag','querystring', 'cookie', 'localStorage',
+         'sessionStorage', 'navigator',
+          'path', 'subdomain'],
+        caches: ['cookie']
+    }
+  });
+
+function LangSwitcher() {
+  const { t } = useTranslation();
+
+  return <h2>{t('Welcome')}</h2>;
+}
 
 export default LangSwitcher;
+
+
+// const LangSwitcher = () => {
+//     return (
+//         <div>
+//             <select placeholder='choose your language'>
+//                 <option value="eng">English</option>
+//                 <option value="fa">فارسی</option>
+//             </select>
+//         </div>
+//     );
+// };
+
+// export default LangSwitcher;
