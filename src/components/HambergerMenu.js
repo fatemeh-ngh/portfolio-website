b64cdc771348bf56ffda1b659d6b297b986791b8
Fatemeh Negahdari <ftm.ngh.2001@gmail.com>
2022-07-25 23:39:17 +0430

    trying to make hamburger menu with styled component or a package

--- src/components/HambergerMenu.js
+++ src/components/HambergerMenu.js
@@ -1,36 +1,81 @@
 import React, {useState} from 'react';
 import styles from '../sass/Hamber.module.scss'
 import { Link } from 'react-router-dom';
+import { Squash as Hamburger } from 'hamburger-react'
 
 //Hook
 import { useTranslation} from "react-i18next";
+import styled from 'styled-components';
 
-
+// const Container = styled.div`
+//     display: flex;
+//     flex-direction: row-reverse;
+//     // position: relative;
+// `
+// const Icon = styled.div`
+//     // background-color: #000;
+//     margin: auto 0;
+//     width: 30px;
+//     height: 20px;
+//     display: none;
+//     flex-direction: column;
+//     justify-content: space-between;
+//     margin-right: 50px;
+//     cursor: pointer;
+//     span{
+//         width: 100%;
+//         height: 4px;
+//         background-color: #fff;
+//     }
+//     @media (max-width: 992px) {
+//             display: flex;
+//     }
+// }
+// `
+const MenuContainer = styled.div`
+        position: absolute;
+        width: 80vw;
+        height: 300px;
+        margin: 70px 80px;
+        background-color: #2552124f;
+        display: flex;
+        flex-direction: column-reverse;
+        justify-content: space-between;
+    a{
+        text-decoration: none;
+        color: $white;
+        width: 100%;
+        height: 60px;
+        text-align: center;
+        background-color: #A6A7AB;
+    }
+    a:hover{
+        background-color: #909296;
+    }
+`
 const HambergerMenu = () => {
 
-    const {t, i18n} = useTranslation();
+    const {t} = useTranslation();
     const [open, setOpen] = useState('true')
     const MenuHandler= () => {
         setOpen(!open);
-
     }
-    
+    console.log(open);
     return (
-        <div onClick={MenuHandler} className={styles.cotainer}>
-            <div className={`${styles.icon} ${open ? styles.close : styles.open}`}>
-                <span className={styles.ihamber1}></span>
-                <span className={styles.ihamber2}></span>
-                <span className={styles.ihamber3}></span>
-            </div>
-            <div className={styles.MenuContainer}>
+        <Container>
+            <Icon open={open} onClick={MenuHandler}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </Icon>
+            <MenuContainer>
                 <Link to="/contact">{t("Contact")}</Link><span> </span>
                 <Link to="/blog">{t("Blog")}</Link><span> </span>
                 <Link to="/shop">{t("Shop")}</Link><span> </span>
                 <Link to="/portfolio">{t("Portfolio")}</Link><span> </span>
                 <Link to="/">{t("Home")}</Link><span></span>
-            </div>
-
-        </div>
+            </MenuContainer>
+        </Container>
 
     );
 };
