08e24816571462d86973048b606a29b6a5241a56
Fatemeh Negahdari <ftm.ngh.2001@gmail.com>
2022-07-26 19:45:41 +0430

    start making landing page and home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,112 +1,38 @@
 import React,{useContext} from 'react';
+import { Link } from 'react-router-dom';
 
 //Images
 // import background from "../assets/images/background.jpg";
 // import styled from 'styled-components';
 
 //Context
-import {Themecontext} from '../contexts/ThemeContext';
+// import {Themecontext} from '../contexts/ThemeContext';
 
 //Styles
 import styles from '../sass/Home.module.scss';
 
 //Hook
 import { useTranslation} from "react-i18next";
-
+// ${theme} 
+// ${i18n.language === 'en' ? styles.english : styles.farsi}
 
 const Home = () => {
 
-        const [theme,] = useContext(Themecontext);
+        // const [theme,] = useContext(Themecontext);
         const {t, i18n} = useTranslation();
 
         return (
-        <div className={`
-        ${styles.container}
-        ${theme} 
-        ${i18n.language === 'en' ? styles.english : styles.farsi}`}>
-            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم 
-                از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها
-                 و متون بلکه روزنامه و مجله در ستون و سطر
-                آنچنان که لازم است، و برای شرایط فعلی تکنولوژی
-                 مورد نیاز، و کاربردهای متنوع با هدف بهبود
-                 ابزارهای کاربردی می باشد، کتابهای زیادی در
-                 شصت و سه درصد گذشته حال و آینده، شناخت فراوان 
-                جامعه و متخصصان را می طلبد، تا با نرم افزارها
-                 شناخت بیشتری را برای طراحان رایانه ای علی 
-                الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی 
-                ایجاد کرد، در این صورت می توان امید داشت که تمام
-                 و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ
-                 به پایان رسد و زمان مورد نیاز شامل حروفچینی
-                 دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل
-                 دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p> 
-            <br />          
-            <br />          
-            <br />          
-            <br />         
-            <p>
-                Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Quo consequuntur iure
-                similique vitae totam ducimus nemo quaerat.
-                Hic explicabo blanditiis nisi perferendis
-                quis natus! Doloremque odio ab nemo, incidunt asperiores cum
-                 quod iure aliquid adipisci sequi debitis vel eius mollitia nobis tempora.
-                Mollitia aut quos amet quasi eius tenetur ut.
-            </p>
-            <br />          
-            <br />          
-            <br />          
-            <p>Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Quo consequuntur iure
-                similique vitae totam ducimus nemo quaerat.
-                Hic explicabo blanditiis nisi perferendis
-                quis natus! Doloremque odio ab nemo, incidunt asperiores cum
-                 quod iure aliquid adipisci sequi debitis vel eius mollitia nobis tempora.
-                Mollitia aut quos amet quasi eius tenetur ut.
-            </p>
-            <br />          
-            <br />          
-            <br />          
-            <p>Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Quo consequuntur iure
-                similique vitae totam ducimus nemo quaerat.
-                Hic explicabo blanditiis nisi perferendis
-                quis natus! Doloremque odio ab nemo, incidunt asperiores cum
-                 quod iure aliquid adipisci sequi debitis vel eius mollitia nobis tempora.
-                Mollitia aut quos amet quasi eius tenetur ut.
-            </p>
-            <br />          
-            <br />          
-            <br />          
-            <p>Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Quo consequuntur iure
-                similique vitae totam ducimus nemo quaerat.
-                Hic explicabo blanditiis nisi perferendis
-                quis natus! Doloremque odio ab nemo, incidunt asperiores cum
-                 quod iure aliquid adipisci sequi debitis vel eius mollitia nobis tempora.
-                Mollitia aut quos amet quasi eius tenetur ut.
-            </p>
-            <br />          
-            <br />          
-            <br />          
-            <p>Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Quo consequuntur iure
-                similique vitae totam ducimus nemo quaerat.
-                Hic explicabo blanditiis nisi perferendis
-                quis natus! Doloremque odio ab nemo, incidunt asperiores cum
-                 quod iure aliquid adipisci sequi debitis vel eius mollitia nobis tempora.
-                Mollitia aut quos amet quasi eius tenetur ut.
-            </p>
-            <br />          
-            <br />          
-            <br />          
-            <p>Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Quo consequuntur iure
-                similique vitae totam ducimus nemo quaerat.
-                Hic explicabo blanditiis nisi perferendis
-                quis natus! Doloremque odio ab nemo, incidunt asperiores cum
-                 quod iure aliquid adipisci sequi debitis vel eius mollitia nobis tempora.
-                Mollitia aut quos amet quasi eius tenetur ut.
-            </p>
+        <div className={styles.container}>
+            <div>
+                <p>{t("INTRODUCTION")}</p>
+                <h1>{t("name")}</h1>
+                <h2>{t("job")}</h2>
+                <p>{t("about")}</p>
+                <div>
+                    <button>{t("downloadcv")}</button>
+                    <Link to='/contact'>{t("Contact")}</Link>
+                </div>
+            </div>
         </div>
     );
 };
