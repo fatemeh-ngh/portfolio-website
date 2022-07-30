d4c4ca3ff4a4fa68e14079c469fd40bf4d33cefa
Fatemeh Negahdari <ftm.ngh.2001@gmail.com>
2022-07-30 20:43:56 +0430

    add elements to contact component and start it

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+//Styles
+import styles from '../sass/Contact.module.scss';
+
 const Contact = () => {
     return (
-        <div>
-            contact
+        <div className={styles.container}>
+            <div>
+                <h1>contact with me</h1>
+            </div>
+            <form className={styles.formContainer}>
+                <input type="text" id='name' /> 
+                <label htmlFor="name">Full Name</label>
+                <input type="text" id='number'/> 
+                <label htmlFor="number">Phone Number</label>
+                <input type="email" id='email' /> 
+                <label htmlFor="email">Email Address</label>
+                <input type="text" id='text' /> 
+                <label htmlFor="text">Message</label>
+                <button type='submit'>Send Message</button>
+            </form>
+            <div className={styles.contactWay}>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     );
 };
